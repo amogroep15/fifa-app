30a05c177f9ddf0f3dd22eedfe3a190cbab65cbd
Rick <rickvanzelst@live.nl>
2019-05-23 11:20:17 +0200

    g

--- FifaBetApp/Form1.Designer.cs
+++ FifaBetApp/Form1.Designer.cs
@@ -219,7 +219,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox teamBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label1; 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
